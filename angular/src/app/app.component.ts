15cc3652822d1483922297571cf07e8401bb2276
diffshare <diffshare@gmail.com>
2019-01-03 11:40:04 +0900

    drag&dropでローカルファイルを暗号化するお試しコードを記述

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FileSystemFileEntry, UploadEvent, UploadFile} from 'ngx-file-drop';
+import {isContentQueryHost} from '@angular/core/src/render3/util';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,55 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular';
+
+  files: UploadFile[] = [];
+  encryptedFiles: ArrayBuffer[] = [];
+  starting: boolean;
+
+  onFileDrop($event: UploadEvent) {
+    console.log($event);
+
+    this.files = this.files.concat($event.files);
+    this.start();
+  }
+
+  start() {
+    if (this.starting) {
+      return;
+    }
+    this.next();
+  }
+
+  next() {
+    if (this.files.length === 0) {
+      this.starting = false;
+      return;
+    }
+
+    const reader = new FileReader();
+    const firstFile = this.files[0].fileEntry as FileSystemFileEntry;
+    firstFile.file(file => {
+      reader.onloadend = ev => {
+        this.encrypt(reader.result as ArrayBuffer);
+      };
+      reader.readAsArrayBuffer(file);
+    });
+  }
+
+  encrypt(data: ArrayBuffer) {
+    window.crypto.subtle.generateKey({
+      name: 'AES-GCM',
+      length: 256
+    }, false, ['encrypt', 'decrypt']).then(key => {
+      return window.crypto.subtle.encrypt({
+        name: 'AES-GCM',
+        iv: window.crypto.getRandomValues(new Uint8Array(12))
+      }, key, data);
+    }).then(value => {
+      console.log(value);
+      this.encryptedFiles.push(value);
+      this.files.shift();
+      this.next();
+    });
+  }
 }
