3c8a780a4f14f67bf988e1921a693802eca9472a
diffshare <diffshare@gmail.com>
2019-01-14 19:05:23 +0900

    fix: 対応が不十分だったので修正

--- angular/src/app/album/album.component.ts
+++ angular/src/app/album/album.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, SecurityContext} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {AngularFireStorage} from '@angular/fire/storage';
 import {HttpClient} from '@angular/common/http';
