2abb94636ea0752b734a20556544de19249857c5
diffshare <diffshare@gmail.com>
2019-01-14 18:16:57 +0900

    refactor: 不要なnpm packageを除去

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,6 +1,4 @@
 import {Component} from '@angular/core';
-import {FileSystemFileEntry, UploadEvent, UploadFile} from 'ngx-file-drop';
-import {AngularFireStorage} from '@angular/fire/storage';
 
 @Component({
   selector: 'app-root',
