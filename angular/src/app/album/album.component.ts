b87ea818763ff2f621c8deb50fa73208d6b5f06d
diffshare <diffshare@gmail.com>
2019-01-14 21:00:55 +0900

    fix: 全画面時はスクロールバーを表示しないように

--- angular/src/app/album/album.component.ts
+++ angular/src/app/album/album.component.ts
@@ -46,10 +46,12 @@ export class AlbumComponent implements OnInit {
 
       if (this.route.firstChild == null) {
         this.currentImageName = null;
+        this.hasChild = false;
         return;
       }
       const photoParams = await this.route.firstChild.snapshot.params;
       this.currentImageName = photoParams.photo_id;
+      this.hasChild = true;
     });
   }
 
@@ -69,6 +71,7 @@ export class AlbumComponent implements OnInit {
 
   keyPromise: PromiseLike<CryptoKey>;
 
+  hasChild = false;
   album_id: string;
   imageList: DecryptedImage[] = [];
   showPhotoDetail = false;
