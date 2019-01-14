8a37ec8d531f7dad54e935fd4f4d066993b0bd68
diffshare <diffshare@gmail.com>
2019-01-14 18:19:22 +0900

    fix: 対応が不完全だったので修正

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,26 +3,25 @@ import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {FileDropModule} from 'ngx-file-drop';
 import {AngularFireModule} from '@angular/fire';
 import {environment} from '../environments/environment';
-import {AngularFirestoreModule} from '@angular/fire/firestore';
-import {AngularFireStorageModule, StorageBucket} from '@angular/fire/storage';
+import {AngularFireStorageModule} from '@angular/fire/storage';
 import { HomeComponent } from './home/home.component';
 import { AlbumComponent } from './album/album.component';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
+import { PhotoComponent } from './photo/photo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AlbumComponent
+    AlbumComponent,
+    PhotoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FileDropModule,
     FormsModule,
     HttpClientModule,
     AngularFireModule.initializeApp(environment.firebase),
