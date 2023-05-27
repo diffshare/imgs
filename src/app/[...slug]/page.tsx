32eb27c0621ca4f09a28df50b2ef15e10ea7ed7d
diffshare <diffshare@gmail.com>
2023-05-27 04:36:20 +0000

    page: 単一フォトの表示に対応

--- src/app/[...slug]/page.tsx
+++ src/app/[...slug]/page.tsx
@@ -4,14 +4,36 @@ import { useEffect, useState } from "react";
 import { importKey, loadFileList, loadImage, storage } from "../../lib/firebase";
 import { DecryptedImage } from "@/lib/decrypted-image";
 import styles from './page.module.css'
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
-export default function Album({ params }: { params: { album_id: string } }) {
-  const album_id = params.album_id;
+export default function Album({ params }: { params: { slug: string[] } }) {
+  const album_id = params.slug[0];
+  const photo_id = params.slug[1];
+  const router = useRouter();
 
   const [loading, setLoading] = useState(false);
   const [fileList, setFileList] = useState<string[]>([]);
   const [imageList, setImageList] = useState<DecryptedImage[]>([]);
+  const [currentImage, setCurrentImage] = useState<DecryptedImage>();
 
+  // カーソルキーによるナビゲーション
+  useEffect(() => {
+    function handler(event: KeyboardEvent) {
+      if (!currentImage) return;
+      const index = fileList.indexOf(currentImage.name);
+      if (event.key === 'ArrowRight') {
+        router.push(`/${album_id}/${fileList[Math.min(fileList.length - 1, index + 1)]}${window.location.hash}`);
+      }
+      if (event.key === 'ArrowLeft') {
+        router.push(`/${album_id}/${fileList[Math.max(0, index - 1)]}${window.location.hash}`);
+      }
+    }
+    window.addEventListener('keydown', handler);
+    return () => window.removeEventListener('keydown', handler);
+  }, [currentImage, album_id, fileList, router]);
+
+  // アルバムの読込
   useEffect(() => {
     const hash = window.location.hash.substring(3);
     async function load() {
@@ -28,9 +50,21 @@ export default function Album({ params }: { params: { album_id: string } }) {
     load();
   }, [album_id]);
 
+  // 単一フォトの読込
+  useEffect(() => {
+    setCurrentImage(imageList.filter(i => i.name === photo_id)[0]);
+  }, [imageList, photo_id]);
+
   return (
     <div>
-      {/* {currentImageName}
+      {currentImage && (
+        <div className={styles.photo}>
+          <Link href={`/${album_id}${window.location.hash}`}>
+            <img src={currentImage.url} />
+          </Link>
+        </div>
+      )}
+      {/* 
       <AppPhoto image={currentImage} album_id={album_id} /> */}
       {/* <label>
         <input type="checkbox" checked={showEdit} onChange={() => setShowEdit(!showEdit)} />
@@ -53,7 +87,9 @@ export default function Album({ params }: { params: { album_id: string } }) {
       <div className={styles["photo-list"]}>
         {imageList.map(image => (
           <div key={image.url}>
-            <img src={image.url} />
+            <Link href={`/${album_id}/${image.name}${window.location.hash}`}>
+              <img src={image.url} />
+            </Link>
             {/* // <img src="image?.url" *ngIf="image?.url" (click)="gotoPhoto(image)" /> */}
             <div>
               <a href={image.originalImageUrl}>{image.name}</a>
